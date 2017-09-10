08a820cb58bafb65c20c0c31706ca8b5ad87fa59
biahv <beatriz.hviana@gmail.com>
2017-09-10 20:06:57 -0300

    Continuaçã

--- src/main/java/com/algaworks/gestaofilme/config/WebSecurityConfig.java
+++ src/main/java/com/algaworks/gestaofilme/config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .logout()
                 .permitAll();
-        //http.csrf().disable();
+        http.csrf().disable();
     }
 
     @Autowired
